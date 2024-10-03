e1fcab3bf7431c35568e6df31eb948908dcebc39
tientran1234 <tranngoctien29112003@gmail.com>
2024-10-03 11:20:31 +0700

    complete fix

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,34 +1,35 @@
-import mysql, { Connection, OkPacket } from 'mysql2'
-import { envConfig } from '~/constants/config'
+import mysql, { Connection, Pool } from 'mysql2'
 import User from '~/schemas/User.schema'
 
 class DatabaseService {
-  private connection: Connection
+  private connection: Pool
   constructor() {
-    this.connection = mysql.createConnection({
-      host: 'bzjyg0s1aiyqx76l27tw-mysql.services.clever-cloud.com',
+    this.connection = mysql.createPool({
+      host: "bzjyg0s1aiyqx76l27tw-mysql.services.clever-cloud.com",
       port: 20345,
-      user: envConfig.dbUserName,
-      password: envConfig.dbPassword,
-      database: envConfig.dbName
+      user: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      connectTimeout: 10000
     })
   }
 
-  connect() {
+  async connect() {
     return new Promise((resolve, reject) => {
-      this.connection.connect((err) => {
+      this.connection.getConnection((err) => {
         if (err) {
           console.error('Lỗi kết nối cơ sở dữ liệu:', err)
           reject(err)
         } else {
           console.log('Kết nối cơ sở dữ liệu thành công')
+          resolve("hi")
         }
       })
     })
   }
   query<T>(sql: string, params?: (number | string)[]): Promise<T> {
     return new Promise((resolve, reject) => {
-      this.connection.query<OkPacket>(sql, params, (err, results) => {
+      this.connection.query(sql, params, (err, results) => {
         if (err) {
           console.error('Lỗi truy vấn:', err)
           reject(err)
