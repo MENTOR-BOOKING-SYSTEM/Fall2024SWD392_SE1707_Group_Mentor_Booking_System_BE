27251cf62ba2c743e58660f63a35c912205f715d
tientran1234 <tranngoctien29112003@gmail.com>
2024-10-13 20:01:04 +0700

    fix slug

--- src/controllers/projects.controller.ts
+++ src/controllers/projects.controller.ts
@@ -13,22 +13,22 @@ export const submitProjectController = async (
   req: Request<ParamsDictionary, any, submitProjectBody>,
   res: Response
 ) => {
-  // const { user_id, role } = req.decoded_authorization
-  // const [isProjectExist] = await databaseService.query<{ projectName: string; status: number }[]>(
-  //   `select p.status,p.projectName,p.status from  ${DatabaseTable.Project} p join ${DatabaseTable.User_Own_Project} up on p.projectID = up.projectID join ${DatabaseTable.User} u on u.userID = up.userID where u.userID = ? `,
-  //   [user_id]
-  // )
-  // if (isProjectExist && isProjectExist.status === ProjectStatus.Pending) {
-  //   throw new ErrorWithStatus<typeof isProjectExist>({
-  //     status: HTTP_STATUS.BAD_REQUEST,
-  //     message: PROJECTS_MESSAGE.CAN_NOT_SEND_MORE_PROJECT,
-  //     data: isProjectExist
-  //   })
-  // }
-  // const result = await projectServices.createProject(req.body, user_id, role as string[])
+  const { user_id, role } = req.decoded_authorization
+  const [isProjectExist] = await databaseService.query<{ projectName: string; status: number }[]>(
+    `select p.status,p.projectName,p.status from  ${DatabaseTable.Project} p join ${DatabaseTable.User_Own_Project} up on p.projectID = up.projectID join ${DatabaseTable.User} u on u.userID = up.userID where u.userID = ? `,
+    [user_id]
+  )
+  if (isProjectExist && isProjectExist.status === ProjectStatus.Pending) {
+    throw new ErrorWithStatus<typeof isProjectExist>({
+      status: HTTP_STATUS.BAD_REQUEST,
+      message: PROJECTS_MESSAGE.CAN_NOT_SEND_MORE_PROJECT,
+      data: isProjectExist
+    })
+  }
+  const result = await projectServices.createProject(req.body, user_id, role as string[])
 
   return res.json({
     message: PROJECTS_MESSAGE.SUBMIT_PROJECT_SUCESSFULLY,
-    // result
+    result
   })
 }
