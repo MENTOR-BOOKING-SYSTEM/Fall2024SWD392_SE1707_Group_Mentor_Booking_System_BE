6cf7381417e4fc96bcc37b2fab9dbffc7e6cb6c5
tsuna11141 <tsuna11141@gmail.com>
2024-10-15 16:12:16 +0700

    66-apii-get-all-semesters-revert

--- src/routes/semester.routes.ts
+++ src/routes/semester.routes.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express'
 import { getAllSemestersController, getSemesterByIdController } from '~/controllers/semester.controller'
-import { accessTokenValidator } from '~/middlewares/users.middlewares'
 import { semesterIdValidator } from '~/middlewares/semester.middlewares'
 
 const semesterRouter = Router()
-
-// Áp dụng middleware accessTokenValidator cho tất cả các route
-semesterRouter.use(accessTokenValidator)
 
 semesterRouter.get('/all', getAllSemestersController)
 semesterRouter.get('/:semesterID', semesterIdValidator, getSemesterByIdController)
