9dd49ee9b98b7cffd0c8e33fa34924bd43d55fc5
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2024-10-25 16:08:21 +0700

    feat: :sparkles: view + edit account

--- src/models/Request/Dashboard.request.ts
+++ src/models/Request/Dashboard.request.ts
@@ -1,15 +1,28 @@
 import { ParamsDictionary } from 'express-serve-static-core'
 
-export interface GetAccountRequestParams extends ParamsDictionary {
+export interface GetAccountsRequestParams extends ParamsDictionary {
   semesterID: string
 }
-
+export interface GetAccountRequestParams extends ParamsDictionary {
+  userID: string
+  semesterID: string
+}
+export interface EditAccountRequestParams extends ParamsDictionary {
+  userID: string
+  semesterID: string
+}
 export interface CreateAccountReqBody {
   firstName: string
   lastName: string
   username: string
   email: string
   password: string
+  roles: number[]
+  avatarUrl: string
+}
+export interface EditAccountReqBody {
+  firstName: string
+  lastName: string
   roles: number[]
   avatarUrl: string
 }
